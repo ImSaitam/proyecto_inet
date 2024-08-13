24d7926f3e22fe746a80e5af0008daacd0f6d57c
ImSaitam <matutedesanto@gmail.com>
2024-08-13 19:28:26 -0300

    barra nav

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,22 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import BarraNav from './Navbar';
 
 export default function Inicio() {
   return (
-    <Navbar className="bg-body-tertiary">
-    <Container>
-      <Navbar.Brand>
-        <img
-          alt=""
-          src="/img/logo.svg"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}
-        Nombre
-      </Navbar.Brand>
-    </Container>
-  </Navbar>
+    <BarraNav />
   );
 }
