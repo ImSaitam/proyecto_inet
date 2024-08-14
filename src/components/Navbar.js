64d568609b4d814b8a683e1a7946e490d560f8b6
ImSaitam <matutedesanto@gmail.com>
2024-08-13 21:08:43 -0300

    carrito y catalogo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../Navbar.css'
+import { FiShoppingCart } from "react-icons/fi";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { Dropdown } from 'react-bootstrap';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 export default function BarraNav() {
   return (
     <Navbar className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#">
+        <Link to={"/"}>
+        <Navbar.Brand>
           <img
             alt=""
             src="/img/logo.svg"
@@ -16,6 +23,17 @@ export default function BarraNav() {
           />{" "}
           Nombre
         </Navbar.Brand>
+        </Link>
+        <Dropdown as={ButtonGroup}>
+            <Link to={"/carrito"}>
+              <Button variant="success">
+                  <FiShoppingCart className='carrito-icon' />
+              </Button>
+            </Link>
+            <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+            <Dropdown.Menu>
+            </Dropdown.Menu>
+        </Dropdown>
       </Container>
     </Navbar>
   );
