9e187d617691c5dc7335b316ecaf418c62479289
Tomás Arias <tomasariascontacto@gmail.com>
2024-08-14 15:26:14 -0300

    Agregue cosas. todo estatico

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,9 +1,97 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BarraNav from './Navbar.js';
+import BarraNav from './Navbar';
 import Footer from './footer.js';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Carousel from 'react-bootstrap/Carousel';
+import imagenEjemplo from '../img/img1.png';
+import '../inicio.css';
 
 export default function Inicio() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
   return (
-    <><BarraNav /><Footer /></>
+    <>
+      <BarraNav />
+      <Carousel activeIndex={index} onSelect={handleSelect} className="custom-carousel">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={imagenEjemplo}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Primera promocion</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={imagenEjemplo}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Segunda promocion</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={imagenEjemplo}
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Tercera promocion</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+      <h2 className="title-1">Calzado destacado</h2>
+
+      <div className="slider-container mt-4">
+        <div className="slider">
+          {[...Array(10)].map((_, idx) => (
+            <Card key={idx} className="slider-card">
+              <Card.Img variant="top" src={imagenEjemplo} />
+              <Card.Body>
+                <Card.Title>Calzado N°1{idx + 1}</Card.Title>
+                <div className="botones-card"> 
+                <Button variant="primary" clasName="gotoButton">Conseguilo</Button>
+                <Button variant="secondary" className="carrito">🛒</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
+      <h2 className="title-1">Camisetas destacadas</h2>
+      <div className="slider-container mt-4">
+        <div className="slider">
+          {[...Array(10)].map((_, idx) => (
+            <Card key={idx} className="slider-card">
+              <Card.Img variant="top" src={imagenEjemplo} />
+              <Card.Body>
+                <Card.Title>Camiseta N°{idx + 1}</Card.Title>
+                <div className="botones-card"> 
+                <Button variant="primary" clasName="gotoButton">Conseguilo</Button>
+                <Button variant="secondary" className="carrito">🛒</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
+      <Footer/>
+    </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../Navbar.css'
-import { FiShoppingCart } from "react-icons/fi";
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Dropdown } from 'react-bootstrap';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-export default function BarraNav() {
+function BasicExample() {
   return (
-    <Navbar className="bg-body-tertiary">
-      <Container>
-        <Link to={"/"}>
-        <Navbar.Brand>
-          <img
-            alt=""
-            src="/img/logo.svg"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{" "}
-          Nombre
-        </Navbar.Brand>
-        </Link>
-        <Dropdown as={ButtonGroup}>
-            <Link to={"/carrito"}>
-              <Button variant="success">
-                  <FiShoppingCart className='carrito-icon' />
-              </Button>
-            </Link>
-            <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
-            <Dropdown.Menu>
-            </Dropdown.Menu>
-        </Dropdown>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#home">Otna Sports</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
+
+export default BasicExample;
