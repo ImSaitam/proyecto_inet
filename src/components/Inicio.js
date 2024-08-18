7aec740f433fe26e3e9e71edbb4b32ed3dc53270
Tomás Arias <tomasariascontacto@gmail.com>
2024-08-18 18:37:04 -0300

    Cambie el inicio a como lo tenemos en el diseño, todo estatico

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -3,37 +3,31 @@ import BarraNav from './Navbar';
 import Footer from './footer.js';
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import Carousel from 'react-bootstrap/Carousel';
 import imagenEjemplo from '../img/img1.png';
 import '../inicio.css';
-import Pagination from 'react-bootstrap/Pagination';
+import Table from 'react-bootstrap/Table';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { IoCartOutline } from "react-icons/io5";
 
 
 
 export default function Inicio() {
   const [index, setIndex] = useState(0);
-  const [activePage, setActivePage] = useState(1);
 
-  const products = [...Array(10)].map((_, idx) => ({
-    id: idx + 1,
-    title: `Producto N°${idx + 1}`,
-    image: imagenEjemplo
-  }));
 
-  const itemsPerPage = 4;
-  const totalPages = Math.ceil(products.length / itemsPerPage);
+  // const products = [...Array(10)].map((_, idx) => ({
+  //   id: idx + 1,
+  //   title: `Producto N°${idx + 1}`,
+  //   image: imagenEjemplo
+  // }));
+
 
   const handleSelect = (selectedIndex) => {
     setIndex(selectedIndex);
   };
 
-  const handlePageChange = (pageNumber) => {
-    setActivePage(pageNumber);
-  };
 
-  const startIdx = (activePage - 1) * itemsPerPage;
-  const selectedProducts = products.slice(startIdx, startIdx + itemsPerPage);
 
   return (
     <>
@@ -77,42 +71,94 @@ export default function Inicio() {
       </Carousel>
 
       
-      <h2 className="title-1">Calzado destacado</h2>
-      <div className="slider-container mt-4">
-        <div className="slider">
-          {selectedProducts.map((product) => (
-            <Card key={product.id} className="slider-card">
-              <Card.Img variant="top" src={product.image} />
-              <Card.Body>
-                <Card.Title>{product.title}</Card.Title>
-                <div className="botones-card"> 
-                  <Button variant="primary" className="gotoButton">Conseguilo</Button>
-                  <Button variant="secondary" className="carrito">🛒</Button>
-                </div>
-              </Card.Body>
-            </Card>
-          ))}
-        </div>
+      <h2 className="title-1">Productos destacados</h2>
+      <div className='contenido-inicio'>
+        <Table striped bordered className='centered-table'>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Descripción</th>
+              <th>Precio</th>
+              <th>Categoría</th>
+              <th>  </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Nike Phato</td>
+              <td>...........</td>
+              <td>ARS $75.000</td>
+              <td>Calzado</td>
+              <td className='actions-inicio'>
+                <ButtonGroup className="button-comprar" aria-label="">
+                  <Button variant="success">Comprar</Button>
+                </ButtonGroup>
+                <ButtonGroup aria-label="button-carrito">
+                  <Button variant="success"><IoCartOutline /></Button>
+                </ButtonGroup>
+              </td>
+            </tr>
+            <tr>
+            <td>Nike Phato</td>
+              <td>...........</td>
+              <td>ARS $75.000</td>
+              <td>Calzado</td>
+              <td className='actions-inicio'>
+                <ButtonGroup className="button-comprar" aria-label="">
+                  <Button variant="success">Comprar</Button>
+                </ButtonGroup>
+                <ButtonGroup aria-label="button-carrito">
+                  <Button variant="success"><IoCartOutline /></Button>
+                </ButtonGroup>
+              </td>
+            </tr>
+            <tr>
+            <td>Nike Phato</td>
+              <td>...........</td>
+              <td>ARS $75.000</td>
+              <td>Calzado</td>
+              <td className='actions-inicio'>
+                <ButtonGroup className="button-comprar" aria-label="">
+                  <Button variant="success">Comprar</Button>
+                </ButtonGroup>
+                <ButtonGroup aria-label="button-carrito">
+                  <Button variant="success"><IoCartOutline /></Button>
+                </ButtonGroup>
+              </td>
+            </tr>
+            <tr>
+            <td>Nike Phato</td>
+              <td>...........</td>
+              <td>ARS $75.000</td>
+              <td>Calzado</td>
+              <td className='actions-inicio'>
+                <ButtonGroup className="button-comprar" aria-label="">
+                  <Button variant="success">Comprar</Button>
+                </ButtonGroup>
+                <ButtonGroup aria-label="button-carrito">
+                  <Button variant="success"><IoCartOutline /></Button>
+                </ButtonGroup>
+              </td>
+            </tr>
+            <tr>
+            <td>Nike Phato</td>
+              <td>...........</td>
+              <td>ARS $75.000</td>
+              <td>Calzado</td>
+              <td className='actions-inicio'>
+                <ButtonGroup className="button-comprar" aria-label="">
+                  <Button variant="success">Comprar</Button>
+                </ButtonGroup>
+                <ButtonGroup aria-label="button-carrito">
+                  <Button variant="success"><IoCartOutline /></Button>
+                </ButtonGroup>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
       </div>
-
-      <Pagination className="justify-content-center mt-4">
-        <Pagination.Prev
-          onClick={() => handlePageChange(activePage > 1 ? activePage - 1 : 1)}
-        />
-        {[...Array(totalPages)].map((_, idx) => (
-          <Pagination.Item
-            key={idx + 1}
-            active={idx + 1 === activePage}
-            onClick={() => handlePageChange(idx + 1)}
-          >
-            {idx + 1}
-          </Pagination.Item>
-        ))}
-        <Pagination.Next
-          onClick={() => handlePageChange(activePage < totalPages ? activePage + 1 : totalPages)}
-        />
-      </Pagination>
-      <Footer/>
+      
+    <Footer/>
     </>
   );
 }
